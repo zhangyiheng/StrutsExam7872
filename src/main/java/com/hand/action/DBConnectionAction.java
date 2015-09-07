2cbc7bfc45b698451c40d8ea6bf672a715f2793a
zhangyiheng <741069187@qq.com>
2015-09-07 22:29:33 +0800

    first commit

--- src/main/java/com/hand/action/DBConnectionAction.java
+++ src/main/java/com/hand/action/DBConnectionAction.java
@@ -1,22 +1,17 @@
 package com.hand.action;
 
 import java.sql.ResultSet;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.hand.bean.Customer;
+import com.hand.dao.CustomerDao;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class DBConnectionAction extends ActionSupport {
 
-	@Override
-	public String execute() throws Exception {
-		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		
-		ResultSet customer = (ResultSet) context.getBean("custom");
-		
-		return SUCCESS;
-	}
+
 
 }
--- src/main/java/com/hand/action/LoginAction.java
+++ src/main/java/com/hand/action/LoginAction.java
@@ -1,5 +1,16 @@
 package com.hand.action;
 
+import java.util.List;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hand.bean.Customer;
+import com.hand.dao.CustomerDao;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport {
@@ -13,12 +24,27 @@ public class LoginAction extends ActionSupport {
 
 	@Override
 	public void validate() {
-		if(username==null||username.trim().equals("")){
+		if (username == null || username.trim().equals("")) {
 			addFieldError("username", "用户不能为空");
 		}
-		if(password==null||password.trim().equals("")){
+		if (password == null || password.trim().equals("")) {
 			addFieldError("password", "密码不能为空");
 		}
+	}
+
+	public String check() {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"ApplicationContext.xml");
+
+		CustomerDao customerss = (CustomerDao) context.getBean("customer");
+		List<Customer> customers = customerss.getCustomer();
+		for (Customer customer : customers) {
+			if (customer.getFirst_name().equals(username)&&customer.getLast_name().equals(password)) {
+				
+				return SUCCESS;
+			}
+		}
+		return ERROR;
 	}
 
 	public String getUsername() {
