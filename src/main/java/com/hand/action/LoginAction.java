774b7e21255e6ca23aca6851ab4e5699b9765dc8
zhangyiheng <741069187@qq.com>
2015-09-08 11:50:59 +0800

    commit

--- src/main/java/com/hand/action/LoginAction.java
+++ src/main/java/com/hand/action/LoginAction.java
@@ -1,9 +1,15 @@
 package com.hand.action;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.ServletActionContext;
 import org.springframework.context.ApplicationContext;
@@ -19,7 +25,52 @@ public class LoginAction extends ActionSupport {
 
 	@Override
 	public String execute() throws Exception {
-		return SUCCESS;
+		String ret = ERROR;
+		Connection conn = null;
+		HttpServletRequest request = ServletActionContext.getRequest();
+		try {
+			
+			
+			String Url="jdbc:mysql://localhost:3306/sakila";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(Url,"root","123456");
+			String sql = "select * from customer";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			List<Customer> customers = new ArrayList<Customer>();
+			HttpSession session = request.getSession();
+			while(rs.next()){
+				Customer customer = new Customer();
+				System.out.println(rs.getString("first_name"));
+				if(username.equals(rs.getString("first_name"))&&password.equals(rs.getString("last_name"))){
+					ret=SUCCESS;
+				}
+				customer.setFirst_name(rs.getString("first_name"));
+				customer.setLast_name(rs.getString("last_name"));
+				customer.setAddress_id(rs.getInt("address_id"));
+				customer.setEmail(rs.getString("email"));
+				customer.setCustomer_id(rs.getInt("customer_id"));
+				customer.setLast_update(rs.getDate("last_update"));
+				
+				customers.add(customer);
+			}
+			session.setAttribute("customers",customers);
+
+			
+		} catch (Exception e) {
+			 ret = ERROR;
+			e.printStackTrace();
+		}finally{
+			if(conn!=null){
+				try{
+					conn.close();
+				}catch(Exception e){
+					
+				}
+			}
+		}
+		return ret;
 	}
 
 	@Override
@@ -32,20 +83,20 @@ public class LoginAction extends ActionSupport {
 		}
 	}
 
-	public String check() {
-		ApplicationContext context = new ClassPathXmlApplicationContext(
-				"ApplicationContext.xml");
-
-		CustomerDao customerss = (CustomerDao) context.getBean("customer");
-		List<Customer> customers = customerss.getCustomer();
-		for (Customer customer : customers) {
-			if (customer.getFirst_name().equals(username)&&customer.getLast_name().equals(password)) {
-				
-				return SUCCESS;
-			}
-		}
-		return ERROR;
-	}
+//	public String check() {
+//		ApplicationContext context = new ClassPathXmlApplicationContext(
+//				"ApplicationContext.xml");
+//
+//		CustomerDao customerss = (CustomerDao) context.getBean("customer");
+//		List<Customer> customers = customerss.getCustomer();
+//		for (Customer customer : customers) {
+//			if (customer.getFirst_name().equals(username)&&customer.getLast_name().equals(password)) {
+//				
+//				return SUCCESS;
+//			}
+//		}
+//		return ERROR;
+//	}
 
 	public String getUsername() {
 		return username;
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,84 @@
+<%@page import="com.hand.bean.Customer"%>
+<%@page import="java.sql.ResultSet"%>
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://"
+			+ request.getServerName() + ":" + request.getServerPort()
+			+ path + "/";
+%>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
+<head>
+<base href="<%=basePath%>">
+
+<title>My JSP 'login.jsp' starting page</title>
+
+<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="expires" content="0">
+<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+<meta http-equiv="description" content="This is my page">
+<!--
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	-->
+
+</head>
 <body>
-<h2>Hello World!</h2>
+<div>
+		<div class="head" style="height: 50px;width: auto;">
+			<p>7872:张艺亨</p>
+			<div class="user"></div>
+		</div>
+		<div class="container" >
+			<div class="leftdiv" border="1px">
+				<ul class="nav nav-pills nav-stacked" >
+					<li><a href="#">Customer管理</a></li>
+					<li><a href="#">Film設置</a></li>
+				</ul>
+			</div>
+			<div class="rightdiv">
+				<table border="1px">
+					<tr><td colspan="7"  align="center">客户管理<button type="button" href="insert.jsp"><a href="insert.jsp">添加</a></button></td></tr>
+					<tr><td>操作</td><td>First Name</td><td>Last Name</td><td>Address</td><td>Email</td><td>CustomerId</td><td>LastUpdate</td></tr>
+					<%
+						List rs = (List)session.getAttribute("customers");
+						//List<Customer> customers = new ArrayList<Customer>();
+						
+						for(Iterator iterator = rs.iterator();iterator.hasNext();){
+							Customer customer = (Customer)iterator.next();
+							out.print("<tr>");
+		  		 			out.print("<td>");
+		  		 			out.print("<a href='#' name = 'bianji'>编辑</a>"+"|"+
+		  		 			"<a href='#' name = '删除'>删除</a>");
+		  		 			out.print("</td>");
+		  		 			out.print("<td>");
+		  		 			out.print(customer.getFirst_name());
+		  		 			out.print("</td>");
+		  		 			out.print("<td>");
+		  		 			out.print(customer.getLast_name());
+		  		 			out.print("</td>");
+		  		 			out.print("<td>");
+		  		 			out.print(customer.getAddress_id());
+		  		 			out.print("</td>");
+		  		 			out.print("<td>");
+		  		 			out.print(customer.getEmail());
+		  		 			out.print("</td>");
+		  		 			out.print("<td>");
+		  		 			out.print(customer.getCustomer_id());
+		  		 			out.print("</td>");
+		  		 			out.print("<td>");
+		  		 			out.print(customer.getLast_update());
+		  		 			out.print("</td>");
+		  					out.print("</tr>");
+						}
+						
+					 %>
+				</table>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
